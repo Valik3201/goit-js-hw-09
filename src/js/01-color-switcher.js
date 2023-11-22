5c1458089c4e0139a8bf36f9a76da0ac3df3ee73
Valik <valik3201@gmail.com>
2023-11-22 10:45:23 +0100

    Added color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let timerId = null;
 
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", () => {
+  toggleButtons();
+  colorSwitcher();
+
+  timerId = setInterval(() => {
+    colorSwitcher();
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  toggleButtons();
+  clearInterval(timerId);
+});
+
+function colorSwitcher() {
+  const bgColor = getRandomHexColor();
+  document.body.style.backgroundColor = bgColor;
+}
+
+function toggleButtons() {
+  startBtn.disabled = !startBtn.disabled;
+  stopBtn.disabled = !stopBtn.disabled;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
