19115adb6d4a57e1c446e874c60ba968d6eaf790
Valik <valik3201@gmail.com>
2023-11-22 18:13:44 +0100

    Added countdown timer functionality

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,12 +5,20 @@ import "flatpickr/dist/flatpickr.min.css";
 // Import Notiflix library
 import Notiflix from "notiflix";
 
-// Import Notiflix library one by one
-import { Notify } from "notiflix/build/notiflix-notify-aio";
-import { Report } from "notiflix/build/notiflix-report-aio";
-import { Confirm } from "notiflix/build/notiflix-confirm-aio";
-import { Loading } from "notiflix/build/notiflix-loading-aio";
-import { Block } from "notiflix/build/notiflix-block-aio";
+// Styles for Notiflix
+Notiflix.Notify.init({
+  fontSize: "1rem",
+  width: "500px",
+  cssAnimationStyle: "from-bottom",
+});
+
+const startBtn = document.querySelector("button[data-start]");
+const resetBtn = document.querySelector("button[data-reset]");
+startBtn.disabled = true;
+resetBtn.disabled = true;
+
+let endDate;
+let intervalId;
 
 const options = {
   enableTime: true,
@@ -18,11 +26,38 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates.length > 0) {
+      const selectedDate = selectedDates[0];
+
+      if (selectedDate > new Date()) {
+        startBtn.disabled = false;
+        resetBtn.disabled = false;
+
+        const formattedDate = new Intl.DateTimeFormat("en-US", {
+          month: "long",
+          day: "numeric",
+          year: "numeric",
+          hour: "numeric",
+          minute: "numeric",
+          hour12: false,
+        }).format(selectedDate);
+
+        Notiflix.Notify.success(`Selected Date: ${formattedDate}`);
+
+        endDate = selectedDate.getTime();
+      } else {
+        startBtn.disabled = true;
+        resetBtn.disabled = true;
+        Notiflix.Notify.failure("Please choose a date in the future");
+      }
+    }
   },
 };
 
 flatpickr("#datetime-picker", options);
+
+document.querySelector("#datetime-picker").value =
+  "Enter a future date to begin the countdown";
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -42,3 +77,56 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+
+function updateCountdown() {
+  const currentDate = new Date().getTime();
+  const timeDifference = endDate - currentDate;
+
+  if (timeDifference <= 0) {
+    clearInterval(intervalId);
+    startBtn.disabled = true;
+    resetBtn.disabled = true;
+
+    document.querySelector("[data-days]").textContent = "00";
+    document.querySelector("[data-hours]").textContent = "00";
+    document.querySelector("[data-minutes]").textContent = "00";
+    document.querySelector("[data-seconds]").textContent = "00";
+
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+  document.querySelector("[data-days]").textContent = days
+    .toString()
+    .padStart(2, "0");
+  document.querySelector("[data-hours]").textContent = hours
+    .toString()
+    .padStart(2, "0");
+  document.querySelector("[data-minutes]").textContent = minutes
+    .toString()
+    .padStart(2, "0");
+  document.querySelector("[data-seconds]").textContent = seconds
+    .toString()
+    .padStart(2, "0");
+}
+
+startBtn.addEventListener("click", function () {
+  intervalId = setInterval(updateCountdown, 1000);
+
+  startBtn.disabled = true;
+  resetBtn.disabled = false;
+});
+
+resetBtn.addEventListener("click", function () {
+  clearInterval(intervalId);
+  startBtn.disabled = true;
+  resetBtn.disabled = true;
+
+  document.querySelector("#datetime-picker").value =
+    "Enter a future date to begin the countdown";
+  document.querySelector("[data-days]").textContent = "00";
+  document.querySelector("[data-hours]").textContent = "00";
+  document.querySelector("[data-minutes]").textContent = "00";
+  document.querySelector("[data-seconds]").textContent = "00";
+});
