761223a02a076f0ee3d5e973ec414b4621ef5b8f
Valik <valik3201@gmail.com>
2023-11-23 22:38:53 +0100

    refactor: updated promise generation functionality

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,7 +4,7 @@ import Notiflix from "notiflix";
 // Styles for Notiflix
 Notiflix.Notify.init({
   fontSize: "1rem",
-  width: "400px",
+  width: "450px",
   cssAnimationStyle: "from-bottom",
   closeButton: false,
   useIcon: false,
@@ -32,27 +32,38 @@ function onSubmitForm(event) {
   } else {
     // Loop to create and handle promises based on user input
     for (let i = 1; i <= amount.value; i++) {
-      setTimeout(() => {
-        // Calculate totalDelay for each iteration
-        let totalDelay = initialDelay + (i - 1) * stepValue;
+      // Record the start time
+      const startTime = performance.now();
 
-        // Creating a promise using the createPromise function
-        createPromise(i, totalDelay)
-          .then(({ position, delay }) => {
-            // Displaying a success notification when the promise is fulfilled
-            Notiflix.Notify.success(
-              `✅ Fulfilled promise ${position} in ${delay}ms`
-            );
-          })
-          .catch(({ position, delay }) => {
-            // Displaying an error notification when the promise is rejected
-            Notiflix.Notify.failure(
-              `❌ Rejected promise ${position} in ${delay}ms`
-            );
-          });
-      }, i * stepValue);
+      // Calculate totalDelay for each iteration
+      let totalDelay = initialDelay + (i - 1) * stepValue;
+
+      // Creating a promise using the createPromise function
+      createPromise(i, totalDelay)
+        .then(({ position, delay }) => {
+          // Calculate the elapsed time
+          const elapsedTime = performance.now() - startTime;
+          // Displaying a success notification when the promise is fulfilled
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms (Actual: ${elapsedTime.toFixed(
+              0
+            )}ms)`
+          );
+        })
+        .catch(({ position, delay }) => {
+          // Calculate the elapsed time
+          const elapsedTime = performance.now() - startTime;
+          // Displaying an error notification when the promise is rejected
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms (Actual: ${elapsedTime.toFixed(
+              0
+            )}ms)`
+          );
+        });
     }
   }
+
+  event.currentTarget.reset();
 }
 
 // Function to create a promise
@@ -61,13 +72,15 @@ function createPromise(position, delay) {
     // Generating a random value to determine whether the promise should be fulfilled or rejected
     const shouldResolve = Math.random() > 0.3;
 
-    if (shouldResolve) {
-      // Fulfill
-      resolve({ position, delay });
-    } else {
-      // Reject
-      reject({ position, delay });
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
   });
 }
 
